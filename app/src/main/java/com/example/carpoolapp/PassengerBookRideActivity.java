56e6f363eefe8f787df954a53b1ed57ddf382c4a
carsondrobe <carsondrobe@gmail.com>
2023-11-27 17:11:47 -0800

    Added a Pickup and Destination AutoComplete Fragment for Passenger book ride

--- app/src/main/java/com/example/carpoolapp/PassengerBookRideActivity.java
+++ app/src/main/java/com/example/carpoolapp/PassengerBookRideActivity.java
@@ -8,7 +8,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.android.volley.BuildConfig;
 import com.google.android.gms.common.api.Status;
+import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
@@ -21,13 +23,37 @@ public class PassengerBookRideActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passenger_book_ride);
+       // Places.initialize(getApplicationContext(), apikey);
 
-
-        // Initialize the AutocompleteSupportFragment.
-        AutocompleteSupportFragment destinationAutocompleteSupportFragment = (AutocompleteSupportFragment)
-                getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+        // Initialize the Pickup AutocompleteSupportFragments
+        AutocompleteSupportFragment pickupAutocompleteSupportFragment = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.activity_passenger_book_ride_frag_PickupLocation);
         // Set Hint for Destination Search Bar
-        destinationAutocompleteSupportFragment.setHint("Enter Destination");
+        pickupAutocompleteSupportFragment.setHint("Pickup Location");
+
+        // Specify the types of place data to return.
+        pickupAutocompleteSupportFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME));
+
+        // Set up a PlaceSelectionListener to handle the response.
+        pickupAutocompleteSupportFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(@NonNull Place place) {
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
+            }
+
+            @Override
+            public void onError(@NonNull Status status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
+
+        // Initialize the destination AutocompleteSupportFragments
+        AutocompleteSupportFragment destinationAutocompleteSupportFragment = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.activity_passenger_book_ride_frag_DestinationLocation);
+        // Set Hint for Destination Search Bar
+        destinationAutocompleteSupportFragment.setHint("Destination Location");
 
         // Specify the types of place data to return.
         destinationAutocompleteSupportFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME));
