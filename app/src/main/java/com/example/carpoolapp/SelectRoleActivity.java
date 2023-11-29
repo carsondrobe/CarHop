a1d3a02aff6820b9cf9bf547540652c695d2ff53
Connor <connor.cahoon@gmail.com>
2023-11-28 20:31:50 -0800

    Added navbar to home 3 screens. Will be moved to the correct screens when they are created. Currently has a bug with repeat selections. Will investigate.

--- app/src/main/java/com/example/carpoolapp/SelectRoleActivity.java
+++ app/src/main/java/com/example/carpoolapp/SelectRoleActivity.java
@@ -1,17 +1,23 @@
 package com.example.carpoolapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class SelectRoleActivity extends AppCompatActivity {
     ImageView passengerButton;
     // TODO: Driver
     ImageView driverButton;
+
+    // NavBar
+    NavigationBarView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,5 +42,41 @@ public class SelectRoleActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        initializeNavBar();
+    }
+    private void initializeNavBar(){
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Intent intent;
+            int selected = bottomNavigationView.getSelectedItemId();
+            Log.d("NavBar", "itemID: "+item.getItemId()+" selected: "+selected);
+            if (item.getItemId() == selected)
+                return false;
+            bottomNavigationView.setSelectedItemId(item.getItemId());
+            selected = bottomNavigationView.getSelectedItemId();
+            Log.d("NavBar", "Now selected: "+selected);
+            switch (item.getItemId()){
+                case R.id.home:
+                    Log.d("NavBar", "to home: ");
+                    intent = new Intent(SelectRoleActivity.this, SelectRoleActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.book:
+                    Log.d("NavBar", "to booking: ");
+                    intent = new Intent(SelectRoleActivity.this, PassengerMainActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.finances:
+                    Log.d("NavBar", "to 'finances': ");
+                    intent = new Intent(SelectRoleActivity.this, DriverMainActivity.class);
+                    startActivity(intent);
+                    break;
+            }
+            return true;
+        });
+        bottomNavigationView.setSelectedItemId(0);
+
     }
 }
